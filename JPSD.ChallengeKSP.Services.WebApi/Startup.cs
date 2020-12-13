ef35af652a3a89bde87a082fc069b6731bb88579
Jean Pierre Soto Delgado <jeanpierre73560@gmail.com>
2020-12-13 17:19:17 -0600

    Implementamos Swagger a Nuestra API

--- JPSD.ChallengeKSP.Services.WebApi/Startup.cs
+++ JPSD.ChallengeKSP.Services.WebApi/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.Logging;
 using JPSD.ChallengeKSP.Services.WebApi.Modules.Mapper;
 using JPSD.ChallengeKSP.Services.WebApi.Modules.Feature;
 using JPSD.ChallengeKSP.Services.WebApi.Modules.Injection;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace JPSD.ChallengeKSP.Services.WebApi
 {
@@ -31,6 +35,34 @@ namespace JPSD.ChallengeKSP.Services.WebApi
             services.AddMapper();
             services.AddFeature(this.Configuration);
             services.AddInjection(this.Configuration);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JPSD Technology Services API ChallengeKSP",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://jeansystem.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Jean Pierre Soto Delgado",
+                        Email = "jeanpierresoto93@gmail.com",
+                        Url = new Uri("https://jeansystem.com"),                        
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://jeansystem.com"),
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +72,15 @@ namespace JPSD.ChallengeKSP.Services.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JPSD API ChallengeKSP V1");
+            });
 
             app.UseRouting();
 
